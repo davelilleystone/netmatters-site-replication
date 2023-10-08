bac7adc2369f05bd470324360c589208fa161e39
Dave Lilleystone <davelilleystone@gmail.com>
2023-10-08 16:20:45 +0100

    crerate large screen sidebar menu

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -2,6 +2,7 @@ const hamburgers = document.querySelectorAll(".top-nav__hamburger");
 const hamburgerIcons = document.querySelectorAll(".top-nav__hamburger-icon");
 const mobileOverlay = document.querySelector(".mobile-nav-overlay");
 const mainContent = document.querySelector(".main-content");
+const sidebar = document.querySelector(".sidebar");
 
 console.log(hamburgers instanceof NodeList);
 
@@ -16,9 +17,19 @@ hamburgers.forEach((el) => {
 });
 
 mobileOverlay.addEventListener("click", () => {
-  mobileOverlay.classList.remove("menu-open");
   hamburgerIcons.forEach((el) => {
     el.classList.remove("menu-open");
   });
   mainContent.classList.remove("menu-open");
+  mobileOverlay.classList.remove("menu-open");
+  sidebar.classList.remove("menu-open");
+});
+
+// adjust z-index of sidebar after slide to left is complete to improve transition effect - snaps
+mainContent.addEventListener("transitionend", (evt) => {
+  if (evt.target.classList.contains("main-content")) {
+    if (getComputedStyle(evt.target).transform != "none") {
+      sidebar.classList.add("menu-open");
+    }
+  }
 });
