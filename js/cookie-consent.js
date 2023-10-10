e629be45ba1843cf1c43b969d1b4564971046832
Dave Lilleystone <davelilleystone@gmail.com>
2023-10-10 20:43:22 +0100

    complete cookie modal and styling

---
+++ js/cookie-consent.js
@@ -0,0 +1,53 @@
+const body = document.querySelector("body");
+const cookieConsentContainer = document.querySelector(".cookie-consent-container");
+const cookieInforPage = document.querySelector(".cookie-info-page");
+const cookieInfoBtn = document.querySelector(".cookie-btn-info");
+const cookieCancelBtn = document.querySelector(".cookie-btn-cancel");
+const cookieAcceptBtn = document.querySelectorAll(".cookie-btn-accept");
+
+const checkCookieConsent = () => {
+  if (!localStorage.getItem("acceptCookies")) {
+    showCookieConsentContainer();
+    setOverflowHidden();
+  }
+};
+
+const setOverflowHidden = () => {
+  body.classList.add("overflow-hidden");
+};
+
+const removeOverflowHidden = () => {
+  body.classList.remove("overflow-hidden");
+};
+
+const showCookieInfoPage = () => {
+  cookieInforPage.classList.remove("hidden");
+};
+
+const hideCookieInfoPage = () => {
+  cookieInforPage.classList.add("hidden");
+};
+
+const showCookieConsentContainer = () => {
+  cookieConsentContainer.classList.remove("hidden");
+};
+
+const hideCookieConsentContainer = () => {
+  cookieConsentContainer.classList.add("hidden");
+};
+
+// add event listeners
+
+cookieAcceptBtn.forEach((btn) => {
+  btn.addEventListener("click", (evt) => {
+    hideCookieConsentContainer();
+    removeOverflowHidden();
+    localStorage.setItem("acceptCookies", true);
+  });
+});
+
+cookieInfoBtn.addEventListener("click", showCookieInfoPage);
+
+cookieCancelBtn.addEventListener("click", hideCookieInfoPage);
+
+window.addEventListener("DOMContentLoaded", checkCookieConsent);
