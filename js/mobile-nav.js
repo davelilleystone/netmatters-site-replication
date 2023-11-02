c879faf8d3e5e5c5c4bbc557d79dd6986e825537
Dave Lilleystone <davelilleystone@gmail.com>
2023-11-02 18:40:59 +0000

    fix issue with screen scrolling to top when mobile hamburger menu is clicked

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -2,6 +2,7 @@ const hamburgers = document.querySelectorAll(".top-nav__hamburger");
 const hamburgerIcons = document.querySelectorAll(".top-nav__hamburger-icon");
 const mobileOverlay = document.querySelector(".mobile-nav-overlay");
 const mainContent = document.querySelector(".main-content");
+const hiddenHeader = document.querySelector(".hidden-header");
 const sidebar = document.querySelector(".sidebar");
 
 // add menu-open classes when hamburger is clicked
@@ -12,6 +13,8 @@ hamburgers.forEach((el) => {
     });
     mobileOverlay.classList.add("menu-open");
     mainContent.classList.add("menu-open");
+    hiddenHeader.classList.add("menu-open");
+    body.style.overflow = "hidden";
   });
 });
 
@@ -23,6 +26,8 @@ mobileOverlay.addEventListener("click", () => {
   mainContent.classList.remove("menu-open");
   mobileOverlay.classList.remove("menu-open");
   sidebar.classList.remove("menu-open");
+  hiddenHeader.classList.remove("menu-open");
+  body.style.overflow = null;
 });
 
 // adjust z-index of sidebar after slide to left is complete to improve transition effect
