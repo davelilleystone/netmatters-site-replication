4404fdb49ad153bb79a0f0ff187a673b278975ca
Dave Lilleystone <davelilleystone@gmail.com>
2023-11-03 12:04:55 +0000

    add sliders to clients and partner section

--- js/slick.js
+++ js/slick.js
@@ -1,17 +1,45 @@
 // start slick slider
 // https://kenwheeler.github.io/slick/
 
-$(document).ready(function () {
-  $(".slick-container").slick({
-    dots: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    pauseOnDotsHover: true,
-    pauseOnHover: true,
-    pauseOnFocus: true,
-    slide: ".slick-item",
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  });
+// $(document).ready(function () {
+$(".slick-container").slick({
+  dots: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  pauseOnDotsHover: true,
+  pauseOnHover: true,
+  pauseOnFocus: true,
+  slide: ".slick-item",
+  slidesToShow: 1,
+  slidesToScroll: 1,
 });
+
+$(".partners").slick({
+  variableWidth: true,
+  autoplay: true,
+  pauseOnHover: false,
+  dots: false,
+  arrows: false,
+  infinite: true,
+  autoplaySpeed: 4000,
+  cssEase: "ease-in-out",
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
+
+$(".clients").slick({
+  variableWidth: true,
+  autoplay: true,
+  pauseOnHover: true,
+  dots: false,
+  arrows: false,
+  infinite: true,
+  autoplaySpeed: 4000,
+  cssEase: "ease-in-out",
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
+// });
